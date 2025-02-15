d044bbe69619bd88682ddad307eb19d7dc329ed3
kosovic-drasko <98913762+kosovic-drasko@users.noreply.github.com>
2025-02-15 22:49:29 +0100

    Ukljnjeno rating i theme

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -15,7 +15,7 @@ const Menu = () => {
   return (
     <div className='flex justify-end gap-3'>
       <nav className='hidden md:flex w-full max-w-xs gap-1'>
-        <ModeToggle />
+        {/* <ModeToggle /> */}
         <Button asChild variant='ghost'>
           <Link href='/cart'>
             <ShoppingCart /> Cart
@@ -30,7 +30,7 @@ const Menu = () => {
           </SheetTrigger>
           <SheetContent className='flex flex-col items-start'>
             <SheetTitle>Menu</SheetTitle>
-            <ModeToggle />
+            {/* <ModeToggle /> */}
             <Button asChild variant='ghost'>
               <Link href='/cart'>
                 <ShoppingCart /> Cart
